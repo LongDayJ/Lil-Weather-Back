dc9ce888d65cd5584ffd3f5bb6d9367bed1d712a
Juarez José <84882813+Juudoido@users.noreply.github.com>
2023-05-29 17:22:31 -0300

    Vam bora

--- src/main/java/br/com/lilweather/utils/Constants.java
+++ src/main/java/br/com/lilweather/utils/Constants.java
@@ -1,5 +1,7 @@
 package br.com.lilweather.utils;
 
 public class Constants {
-
+    public static final String APIkey = "0c09765e328574bfbec932547d0f39fd";
+    public static final String APIurl = "https://api.openweathermap.org/data/2.5/forecast?q=";
+    public static final String APIlang = "&lang=pt_br";
 }
